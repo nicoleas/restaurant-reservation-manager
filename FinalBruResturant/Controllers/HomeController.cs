e4870871d85a741ac9945ffaf8160ed27c645e11
Nicole Armstrong-Smith <nickcolelauren@outlook.com>
2018-01-03 17:41:31 -0500

    Reservations page PRE Entity Framework

--- FinalBruResturant/Controllers/HomeController.cs
+++ FinalBruResturant/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinalBruResturant.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,63 @@ namespace FinalBruResturant.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            return View("MainPage");
+        }
 
-            return View();
+        [HttpGet]
+        public ActionResult ReservationsPage()
+        {
+            return View("ReservationsPage");
+        }
+
+        [HttpPost]
+        public ActionResult ReservationsPage(Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+
+                String sender = Request.Form["submit"]; //store retrieved values from form
+
+                //ENTITY FRAMEWORK
+                /*
+                Student student = new Student();
+
+                student.StudentId = studentId + 1;
+                student.Name = studentResponse.Name;
+                student.Email = studentResponse.Email;
+                student.Phone = studentResponse.Phone;
+                student.Address = studentResponse.Address;
+                student.TechnicalInterest = studentResponse.TechnicalInterest.ToString();
+                student.SocialNetworkInterest = studentResponse.SocialNetworkInterest;
+
+                Attend attend = new Attend();
+
+                attend.Id = attendId + 1;
+                attend.Student = student.StudentId;
+                switch (studentResponse.WillAttend)
+                {
+                    case true:
+                        attend.AcceptRegret = "Accept";
+                        break;
+                    case false:
+                        attend.AcceptRegret = "Regret";
+                        break;
+                    default:
+                        attend.AcceptRegret = "Regret";
+                        break;
+                }
+
+                client.InsertIntoDB(student, attend);
+                */
+
+                return View("Thanks", reservation);
+
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
         }
     }
 }
---
+++ FinalBruResturant/Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalBruResturant.Models
+{
+    public class Reservation
+    {
+        //public string stringMonth = "";
+        [Required(ErrorMessage = "Please pick a date and time")]
+        public DateTime DateTime { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        //[Required(ErrorMessage = "Please pick a date")]
+        //public string Date { get; set; }
+        //[Required(ErrorMessage = "Please choose a time")]
+        //public string Time { get; set; }
+        [Required(ErrorMessage = "Please select the number of people")]
+        public int NumPeople { get; set; }
+    }
+}
